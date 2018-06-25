b515cc475ce6fb5e15f908f6ca6e3ab8a69d6a5a
Kathryn Lawton <klawton16@gmail.com>
2018-06-25 10:27:04 -0500

    added second linq method

--- LINQPractice/Program.cs
+++ LINQPractice/Program.cs
@@ -10,15 +10,35 @@ namespace LINQPractice
 	{
 		static void Main(string[] args)
 		{
+
 			List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
 
-			var resultWords = words.Where(w => w.Contains("th"));
-			foreach(string result in resultWords)
+				var resultWords = words.Where(w => w.Contains("th"));
+				foreach (string result in resultWords)
+				{
+					Console.WriteLine(result);
+				}
+
+			Console.WriteLine();
+			Console.WriteLine();
+
+			List<string> names = new List<string>() { "Mike", "Dan", "Scott", "Nick", "Mike" };
+
+			IEnumerable<string> distinctNames = names.Distinct();
+			foreach(string result in distinctNames)
 			{
 				Console.WriteLine(result);
 			}
 
 			Console.ReadLine();
+
+			List<string> classGrades = new List<string>()
+			{
+				"80, 100, 92, 89, 65",
+				"93, 81, 78, 84, 69",
+				"73, 88, 83, 99, 64",
+				"98, 100, 66, 74, 55"
+			};
 		}
 		
 	}
